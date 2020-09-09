75787fac2891b7fac170fee04d2f5cd631ca6527
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-09-09 16:32:07 -0300

    segunda parte das confs do trab

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 // import { BrowserRouter } from 'react-router-dom';
 // import Routes from './routes';
-// import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/Global';
 
 const App: React.FC = () => (
     <>
         <h1>Hello World</h1>
+        <GlobalStyle />
   </>
 );
 
