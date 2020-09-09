269914c33598503c6927ed8fad5c7684efaf79c3
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-09-09 17:28:12 -0300

    visual da primeira pagina ok, seguindo o tuto do prof

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 // import { BrowserRouter } from 'react-router-dom';
 // import Routes from './routes';
 import GlobalStyle from './styles/Global';
+import SiginIn from './pages/SiginIn';
+
 
 const App: React.FC = () => (
     <>
-        <h1>Hello World</h1>
+        <SiginIn />
         <GlobalStyle />
   </>
 );
