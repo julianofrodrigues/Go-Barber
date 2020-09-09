75356413ce9b901ccb38a43a38064f27d40f1644
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-09-09 16:19:15 -0300

    primeiras confs feitas

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { BrowserRouter } from 'react-router-dom';
+// import Routes from './routes';
+// import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+    <>
+        <h1>Hello World</h1>
+  </>
+);
+
+export default App;
