49082fc6fb17d8875505fda703a99e2bf57598d5
Juliano Ferreira Rodrigues <julianof.rodrigues@hotmail.com>
2020-09-10 15:51:49 -0300

    parte 5 - passando  e testando dados por input feitas

--- front/src/pages/SiginUp/index.tsx
+++ front/src/pages/SiginUp/index.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SiginIn: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
-            <form>
-                <h1>Faça o seu cadastro</h1>
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-Mail" />
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-                <Button type="submit">Entrar</Button>
-            </form>
-            <a href="login">
-                <FiArrowLeft />
-                Voltar para Logon
-            </a>
-        </Content>
-    </Container>
-)
+const SiginIn: React.FC = () => {
+    function handleSubmit(data: object): void{ 
+        console.log(data);
+    }
+    return(
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça o seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-Mail" />
+                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="login">
+                    <FiArrowLeft />
+                    Voltar para Logon
+                </a>
+            </Content>
+        </Container>
+    );
+}
 
 export default SiginIn;
